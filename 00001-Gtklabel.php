2e282f2d473d5167d5e8214bb2be8916fe4bdf0d
Bruno Pitteli Gonçalves <scorninpc@gmail.com>
2023-02-09 14:48:02 -0300

    add comments

--- 00001-Gtklabel.php
+++ 00001-Gtklabel.php
@@ -5,9 +5,13 @@ Gtk::init();
 $window = new \GtkWindow();
 $window->set_size_request(500, 500);
 
+// create the label
 $label = new \GtkLabel("My first line\nThis is the second line");
+
+// justity to right
 $label->set_justify(GtkJustification::RIGHT);
 
+// add label to window
 $window->add($label);
 
 $window->show_all();
