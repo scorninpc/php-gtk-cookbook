ec45d95efc58cd0eb0a42740ac57d8071cba1036
Bruno Pitteli Gonçalves <scorninpc@gmail.com>
2023-02-10 09:09:11 -0300

    add example to how to center multilines on GtkLabel

---
+++ 00003-Gtklabel.php
@@ -0,0 +1,16 @@
+<?php
+
+Gtk::init();
+
+$window = new \GtkWindow();
+$window->set_size_request(500, 500);
+
+// create the label and center
+$label = new \GtkLabel("My first line\nThis is the second line");
+$label->set_justify(GtkJustification::CENTER);
+
+// add label to window
+$window->add($label);
+
+$window->show_all();
+Gtk::main();
