1d06b0ec7a3747a338402db8e449f497b20786f4
Bruno Pitteli Gonçalves <scorninpc@gmail.com>
2023-02-10 08:41:56 -0300

    add example to add data to GtkTreeview

---
+++ 00002-GtkTreeView.php
@@ -0,0 +1,41 @@
+<?php
+
+Gtk::init();
+
+$window = new \GtkWindow();
+$window->set_size_request(500, 500);
+
+// create the treeview
+$treeview = new GtkTreeView();
+$scroll = new GtkScrolledWindow();
+$scroll->add($treeview);
+
+	// columns
+	$column1 = new GtkTreeViewColumn("Name", new GtkCellRendererText(), "text", 0);
+	$column1->set_sort_column_id(0);
+	$treeview->append_column($column1);
+
+	$column2 = new GtkTreeViewColumn("Phone", new GtkCellRendererText(), "text", 1);
+	$column2->set_sort_column_id(1);
+	$treeview->append_column($column2);
+
+	$column3 = new GtkTreeViewColumn("Genre", new GtkCellRendererText(), "text", 2);
+	$column3->set_sort_column_id(2);
+	$treeview->append_column($column3);
+
+// create model
+$model = new GtkListStore(GObject::TYPE_STRING, GObject::TYPE_STRING, GObject::TYPE_STRING);
+$treeview->set_model($model);
+
+	// add values
+	$model->append(["Bruno", "+55 43 9 9282-2039", "M"]);
+	$model->append(["Natália", "+55 17 8273-0293", "F"]);
+	$model->append(["Maria", "+49 4190-8140", "F"]);
+	$model->append(["Sven", "+49 0291-2450", "M"]);
+
+
+// add scrooled panel to window
+$window->add($scroll);
+
+$window->show_all();
+Gtk::main();
