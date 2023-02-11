43ccaf957aae42e4648cf742ff8bc4336286cfcf
Bruno Pitteli Gonçalves <scorninpc@gmail.com>
2023-02-10 21:46:36 -0300

    How to use markup in GtkLabel

---
+++ 00012.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * PHP-GTK Cookbook
+ * 
+ * https://andor.com.br/php-gtk/cook/how-to-use-markup-in-gtklabel
+ */
+
+Gtk::init();
+
+$window = new \GtkWindow();
+$window->set_size_request(500, 500);
+
+// create the label
+$label = new \GtkLabel("<u>underline</u> <i>italic</i> <b>bold</b> <span size=\"xx-large\" foreground=\"blue\">xx-large</span> <span size=\"small\" foreground=\"#00ff00\">small</span>");
+$label->set_use_markup(TRUE);
+
+// or create the label
+// $label2 = new \GtkLabel();
+$label2->set_markup("<u>underline</u> <i>italic</i> <b>bold</b> <span size=\"xx-large\" foreground=\"blue\">xx-large</span> <span size=\"small\" foreground=\"#00ff00\">small</span>");
+
+// add button to window
+$window->add($label2);
+
+$window->show_all();
+Gtk::main();
