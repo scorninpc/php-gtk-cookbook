54260ff632c32a0c42fb037dadf587af76c70b5f
Bruno Pitteli Gonçalves <scorninpc@gmail.com>
2023-02-26 13:17:39 -0300

    How to set color on GtkTreeView column header with CSS

--- 00033.php
+++ 00033.php
@@ -3,7 +3,7 @@
 /**
  * PHP-GTK Cookbook
  * 
- * https://andor.com.br/php-gtk/cook/how-to-set-color-on-gtktreeview-column-header
+ * https://andor.com.br/php-gtk/cook/how-to-set-color-on-gtktreeview-column-header-with-css
  */
 Gtk::init();
 
@@ -24,17 +24,19 @@ $vbox->pack_start($scroll, TRUE, TRUE, 0);
 	// columns
 	$renderer = new GtkCellRendererText();
 	$column1 = new GtkTreeViewColumn("Name", $renderer, "text", 0);
+	$column1->get_button()->set_name("my_column1"); // add custom name to button inside column
 	$treeview->append_column($column1);
  
 	$renderer = new GtkCellRendererText();
-	$renderer->set_property("xalign", 0.5);
 	$column2 = new GtkTreeViewColumn("Phone", $renderer, "text", 1);
+	$column2->get_button()->set_name("my_column2"); // add custom name to button inside column
 	$treeview->append_column($column2);
  
 	$renderer = new GtkCellRendererText();
-	$renderer->set_property("xalign", 1);
 	$column3 = new GtkTreeViewColumn("Genre", $renderer, "text", 2);
+	$column3->get_button()->set_name("my_column3"); // add custom name to button inside column
 	$treeview->append_column($column3);
+
  
 // create model
 $model = new GtkListStore(GObject::TYPE_STRING, GObject::TYPE_STRING, GObject::TYPE_STRING);
@@ -49,6 +51,29 @@ $treeview->set_model($model);
 
 // add to window
 $window->add($vbox);
+
+// create and add CSS
+$css_provider = new GtkCssProvider();
+$css_provider->load_from_data("
+	#my_column1 {
+		background: #1a87ce;
+		border: none;
+		color: #000;
+	}
+	#my_column2 {
+		background: #cd9c41;
+		border: none;
+		color: #000;
+	}
+	#my_column3 {
+		background: #c54e48;
+		border: none;
+		color: #000;
+	}
+");
+ 
+$style_context = new GtkStyleContext();
+$style_context->add_provider_for_screen($css_provider, 600);
 
 // connect to signal that close program
 $window->connect("destroy", function() {
