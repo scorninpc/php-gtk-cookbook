16c95f55e8cc43447e169829f153157979f92952
Bruno Pitteli Gonçalves <scorninpc@gmail.com>
2024-04-13 23:56:36 -0300

    Create 00049.php

---
+++ 00049.php
@@ -0,0 +1,35 @@
+<?php
+
+	/**
+	 * PHP-GTK Cookbook
+	 * 
+	 * https://andor.com.br/php-gtk/cook/working-with-gtknotebook-and-tabs
+	 */
+	Gtk::init();
+
+
+	$window = new GtkWindow();
+	$window->set_size_request(500, 500);
+	$window->set_title("PHP-GTK");
+
+	// create the notebook
+	$notebook = new GtkNotebook();
+	$win->add($notebook);
+	
+	// add page 1
+	$notebook->append_page(GtkButton::new_with_label("Container 1"), new GtkLabel("Page 1"));
+
+	// add page 2
+	$notebook->append_page(GtkButton::new_with_label("Container 2"), new GtkLabel("Page 2"));
+	
+	// add to window
+	$window->add($vbox);
+
+	// connect to signal that close program
+	$window->connect("destroy", function() {
+		Gtk::main_quit();
+	});
+
+	// show all and start
+	$window->show_all();
+	Gtk::main();
